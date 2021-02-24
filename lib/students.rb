4002cdac634fe888846664f80fcc5371a2da5d54
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2021-02-24 16:37:52 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,7 +9,7 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM student;"
+  "SELECT AVG(average_student_gpa) FROM student;"
 end
 
 def total_tardies_for_all_students
